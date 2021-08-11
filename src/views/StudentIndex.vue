c2d1ae4f878739f1672efc70c32a590ffb8bfc9e
MEG HOWE <howem9@gmail.com>
2021-08-11 15:14:09 -0400

    writes template for students index page

---
+++ src/views/StudentIndex.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="students-index">
+    <div v-for="student in students" :key="student.id">
+      <router-link v-bind:to="`/students/${student.id}`">
+        <img :src="student.photo" alt="no image found" />
+        <h2>{{ student.first_name }} {{ student.last_name }}</h2>
+        <p>{{ student.short_bio }}</p>
+      </router-link>
+    </div>
+  </div>
+</template>
+

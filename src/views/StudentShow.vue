e9641066ffed3c6eb36386caa7d0a2cb43d38c90
Ryan Searls <Ryan.searls@gmail.com>
2021-08-12 15:28:21 -0500

    added method and created to student show and added keys to all v-for

--- src/views/StudentShow.vue
+++ src/views/StudentShow.vue
@@ -11,24 +11,24 @@
       <p>{{ student.website }}</p>
       <p>{{ student.github_url }}</p>
     </div>
-    <div v-for="experience in experiences">
+    <div v-for="experience in experiences" :key="experience.id">
       <p>{{ experience.start_date }}</p>
       <p>{{ experience.end_date }}</p>
       <p>{{ experience.job_title }}</p>
       <p>{{ experience.company }}</p>
       <p>{{ experience.details }}</p>
     </div>
-    <div v-for="education in educations">
+    <div v-for="education in educations" :key="education.id">
       <p>{{ education.start_date }}</p>
       <p>{{ education.end_date }}</p>
       <p>{{ education.degree }}</p>
       <p>{{ education.university_name }}</p>
       <p>{{ education.details }}</p>
     </div>
-    <div v-for="skill in skills">
+    <div v-for="skill in skills" :key="skill.id">
       <p>{{ skill.name }}</p>
     </div>
-    <div v-for="capstone in capstones">
+    <div v-for="capstone in capstones" :key="capstone.id">
       <p>{{ capstone.name }}</p>
       <p>{{ capstone.description }}</p>
       <p>{{ capstone.url }}</p>
@@ -43,14 +43,25 @@ import axios from "axios";
 export default {
   data: function () {
     return {
-      students: [],
+      student: {},
+      experiences: [],
+      educations: [],
+      skills: [],
+      capstones: [],
     };
   },
+  created: function () {
+    this.studentShow();
+  },
   methods: {
-    indexStudents: function () {
-      axios.get("http://localhost:3000/students").then((response) => {
-        this.students = response.data;
-        console.log("all students", this.students);
+    studentShow: function () {
+      axios.get("http://localhost:3000/students" + this.$route.params.id).then((response) => {
+        this.student = response.data;
+        this.experiences = this.student.experiences;
+        this.educations = this.student.educations;
+        this.skills = this.student.skills;
+        this.capstones = this.student.capstones;
+        console.log(response.data);
       });
     },
   },
